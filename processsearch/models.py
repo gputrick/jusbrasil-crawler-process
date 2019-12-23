381e16c07bba1956ee145777ffdcb5594babd386
Gabriel Putrick <gputrick@gmail.com>
2019-12-23 00:40:28 -0300

    Added index to process_number and created environment file

--- processsearch/models.py
+++ processsearch/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
+
 class Process(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
-    process_number = models.CharField(max_length=255, unique=True)
+    process_number = models.CharField(db_index=True, max_length=255, unique=True)
     kind = models.TextField()
     area = models.TextField()
     subject = models.TextField()
     distribuition = models.TextField()
     judge = models.TextField()
     action_value = models.TextField()
+
+    class Meta:
+        indexes = (
+            models.Index(fields=['process_number']),
+        )
 
 class Move(models.Model):
     date = models.DateField()
