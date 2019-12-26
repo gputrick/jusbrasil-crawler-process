f7c5299c519ad2dfc96e344329b51a035e8267c3
Gabriel Putrick <gputrick@gmail.com>
2019-12-25 23:34:48 -0300

    Changed sqlite3 by postgres

--- backend/crawlerprocess/settings.py
+++ backend/crawlerprocess/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'crawlerprocess.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crawler',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432'
     }
 }
 
--- backend/django.dockerfile
+++ backend/django.dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.6-alpine
 ENV PYTHONUNBUFFERED 1
 COPY .  /var/www/
-WORKDIR /var/www/backend 
+WORKDIR /var/www/backend
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev 
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-ENTRYPOINT python manage.py runserver 0.0.0.0:8000
+ENTRYPOINT python manage.py migrate && python manage.py runserver 0.0.0.0:8000
